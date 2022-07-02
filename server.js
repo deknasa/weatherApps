8095cc2018f192ce37055175d9a8c339826f8c4d
deknasa <80618997+deknasa@users.noreply.github.com>
2022-07-02 16:06:40 +0700

    first commit weather app

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const apiKey = 'e9154b392bd9028fde91ceb01a583241';
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended:true }))
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res) {
+    res.render("index", { weather:null, error:null });
+})
+
+app.post('/', function(req, res) {
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    request(url, function(err, response, body) {
+        if(err) {
+            res.render('index', { weather: null, error: 'Error, Please try again' });
+        } else {
+            let weather = JSON.parse(body)
+            if (weather.main == undefined) {
+                res.render('index', { weather: null, error: 'Error, Please try again' });
+            } else{
+                let weatherText = `It's ${weather.main.temp} degress in ${weather.name}!`;
+                res.render('index', { weather: weatherText, error: null });
+            }
+        }
+    })
+})
+
+app.listen(3000, function() {
+    console.log("weather app listening on port 3000");
+})
