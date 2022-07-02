45d363aa6e05f31ce294fd18ef73adaa91251721
deknasa <80618997+deknasa@users.noreply.github.com>
2022-07-02 17:21:03 +0700

    update

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.post('/', function(req, res) {
     })
 })
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("weather app listening on port 3000");
 })
